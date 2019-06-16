b15ef3ea49ca58f080fe11745249b641208da4a6
Brandon Dixon <bdixon850@yahoo.com>
2019-06-16 18:20:43 -0400

    setting up routes

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb://bdixon850:senior07@ds137827.mlab.com:37827/heroku_bggk6k4x"
+    mongoURI: "mongodb://bdixon850:senior07@ds137827.mlab.com:37827/heroku_bggk6k4x",
+    secretOrKey: "secret"
 };
